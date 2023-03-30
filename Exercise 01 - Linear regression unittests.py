36424344707e73083aa5bba42fa9dfeab98548f5
Fredrik Wahlberg (VBOX) <fredrik.wahlberg@it.uu.se>
2023-03-30 16:41:42 +0200

    First version of the first exercise's test suite

--- Exercise 01 - Linear regression unittests.py
+++ Exercise 01 - Linear regression unittests.py
@@ -2,24 +2,108 @@ import unittest
 from exercise1 import LinearRegression
 import numpy as np
 
-class TestLinearRegression(unittest.TestCase):
+class TestLinearRegressionFirstOrder(unittest.TestCase):
 
     def setUp(self):
-        self.linreg = LinearRegression()
+        self.linreg = LinearRegression(n_order=1)
+
+    def test_instantiation(self):
+        self.assertIsInstance(self.linreg.loss, list)
+        self.assertTrue(self.linreg.theta is not None)
+        self.assertEqual(len(self.linreg.theta), 2, "Wrong number of parameters for the given order")
+        self.assertIsInstance(self.linreg.n_max_iterations, int)
 
     def test_single_prediction(self):
-        X = np.asarray([3])
-        self.linreg.slope = 2
-        self.linreg.intercept = 1
+        X = np.vstack([3])
+        self.linreg.theta[0] = 2
+        self.linreg.theta[1] = 1
         pred = self.linreg.predict(X)
-        self.assertEqual(pred, 7, "predict() returned %s" % pred)
+        self.assertEqual(pred[0], 5, "predict() returned %s" % pred)
 
-    # def test_several_predictions(self):
-    #     pass
+    def test_several_predictions(self):
+        X = np.vstack([1, 2, 3])
+        self.linreg.theta[0] = 2
+        self.linreg.theta[1] = 1
+        pred = self.linreg.predict(X)
+        self.assertEqual(pred[0], 3, "predict() returned %s" % pred)
+        self.assertEqual(pred[1], 4, "predict() returned %s" % pred)
+        self.assertEqual(pred[2], 5, "predict() returned %s" % pred)
 
-    # def test_fit(self):
-        # self.assertTrue(len(self.linreg.loss_)>0)
-    #     self.assertEqual(self.calculation.get_difference(), 6, 'The difference is wrong.')
+    def test_fit(self):
+        X = np.vstack(np.linspace(-5, 5, num=11))
+        y = X*3 - 2
+        old_score = self.linreg.score(X, y)
+        self.assertTrue(len(self.linreg.loss) == 0, "List of loss scores should be empty at start")
+        self.linreg.fit(X, y)
+        self.assertTrue(len(self.linreg.loss) > 0, "No loss values recorded")
+        self.assertGreater(old_score, self.linreg.score(X, y), 'A trained model should have a lower loss than an untrained.')
+
+    def test_score(self):
+        X = np.vstack(np.linspace(-5, 5, num=11))
+        y = X*3 - 2
+        self.linreg.theta[0] = -2
+        self.linreg.theta[1] = 3
+        self.assertAlmostEqual(self.linreg.score(X, y), 0, 'The loss should be zero here.')
+        self.linreg.theta[0] = -1
+        self.assertAlmostEqual(self.linreg.score(X, y), 11, 'There should be some loss here.')
+        self.linreg.theta[0] = 0
+        self.assertAlmostEqual(self.linreg.score(X, y), 11*2**2, 'Are you using square loss?')
+
+class TestLinearRegressionSecondOrder(unittest.TestCase):
+
+    def setUp(self):
+        self.linreg = LinearRegression(n_order=2)
+
+    def test_instantiation(self):
+        self.assertIsInstance(self.linreg.loss, list)
+        self.assertTrue(self.linreg.theta is not None)
+        self.assertEqual(len(self.linreg.theta), 3, "Wrong number of parameters for the given order")
+        self.assertIsInstance(self.linreg.n_max_iterations, int)
+
+    def test_single_prediction(self):
+        X = np.vstack([3])
+        self.linreg.theta[0] = 2
+        self.linreg.theta[1] = 1
+        self.linreg.theta[2] = 0
+        pred = self.linreg.predict(X)
+        self.assertEqual(pred[0], 5, "predict() returned %s" % pred)
+        X = np.vstack([4])
+        self.linreg.theta[0] = 2
+        self.linreg.theta[1] = 1
+        self.linreg.theta[2] = 3
+        pred = self.linreg.predict(X)
+        self.assertEqual(pred[0], 2+1*4+3*4**2, "predict() returned %s" % pred)
+
+    def test_several_predictions(self):
+        X = np.vstack([1, 2, 3])
+        self.linreg.theta[0] = 2
+        self.linreg.theta[1] = 1
+        self.linreg.theta[2] = 0
+        pred = self.linreg.predict(X)
+        self.assertEqual(pred[0], 3, "predict() returned %s" % pred)
+        self.assertEqual(pred[1], 4, "predict() returned %s" % pred)
+        self.assertEqual(pred[2], 5, "predict() returned %s" % pred)
+
+    def test_fit(self):
+        X = np.vstack(np.linspace(-5, 5, num=11))
+        y = X*3 - 2
+        old_score = self.linreg.score(X, y)
+        self.assertTrue(len(self.linreg.loss) == 0, "List of loss scores should be empty at start")
+        self.linreg.fit(X, y)
+        self.assertTrue(len(self.linreg.loss) > 0, "No loss values recorded")
+        self.assertGreater(old_score, self.linreg.score(X, y), 'A trained model should have a lower loss than an untrained.')
+
+    def test_score(self):
+        X = np.vstack(np.linspace(-5, 5, num=11))
+        y = X*3 - 2
+        self.linreg.theta[0] = -2
+        self.linreg.theta[1] = 3
+        self.linreg.theta[2] = 0
+        self.assertAlmostEqual(self.linreg.score(X, y), 0, 'The loss should be zero here.')
+        self.linreg.theta[0] = -1
+        self.assertAlmostEqual(self.linreg.score(X, y), 11, 'There should be some loss here.')
+        self.linreg.theta[0] = 0
+        self.assertAlmostEqual(self.linreg.score(X, y), 11*2**2, 'Are you using square loss?')
 
 
 if __name__ == '__main__':
